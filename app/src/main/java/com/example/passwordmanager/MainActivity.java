6ffec881d0e744612c3a4023b0c806e954ee2b39
Berkay-ozturk2 <24181616057@gazi.edu.tr>
2026-01-26 19:55:50 +0300

    kategori oluşturma eklencesi

--- app/src/main/java/com/example/passwordmanager/MainActivity.java
+++ app/src/main/java/com/example/passwordmanager/MainActivity.java
@@ -6,6 +6,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
 public class MainActivity extends AppCompatActivity {
     private AppDatabase db;
